5f6bb737275eaca4c2db3d78a90ab7eecdd6eec3
Володимир Буць <volodymyrbuts@MacBook-Pro-Volodimir.local>
2020-08-16 01:10:58 +0300

    Updates

---
+++ precache-manifest.2ed9f3ec661aa32577def9064c64106b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e30439e8c3b5e33cbc5445ddf571a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0e42e5d78a8638ddb6",
+    "url": "/static/js/2.25c47dd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25c47dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e4c86390b08874dea1",
+    "url": "/static/js/main.156ac337.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
